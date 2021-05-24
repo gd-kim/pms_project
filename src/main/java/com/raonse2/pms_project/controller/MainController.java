c6b2d6dfb26cfd2382c52c26c64dd9dada924661
gdkim <oyammo@hanmail.net>
2021-05-24 15:54:49 +0900

    11commit - tr 클릭시 페이지 이동

--- src/main/java/com/raonse2/pms_project/controller/MainController.java
+++ src/main/java/com/raonse2/pms_project/controller/MainController.java
@@ -6,6 +6,7 @@ import com.raonse2.pms_project.service.Project_viewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -33,6 +34,22 @@ public class MainController {
         mv.setViewName("tables");
         List<Project_viewTable> pjList = project_viewService.findByAll();
         mv.getModelMap().addAttribute("pjList", pjList);
+        return mv;
+    }
+
+    @RequestMapping("/pjt-detail/{pjtcode}")
+    public ModelAndView pjtDetail(ModelAndView mv, @PathVariable("pjtcode") String pjtcode){
+        System.out.println("pjtcode:::: "+ pjtcode);
+
+        mv.setViewName("detail");
+        mv.addObject("pjtcode",pjtcode);
+
+        return mv;
+    }
+
+    @RequestMapping("/login")
+    public ModelAndView login(ModelAndView mv){
+        mv.setViewName("login");
         return mv;
     }
 
