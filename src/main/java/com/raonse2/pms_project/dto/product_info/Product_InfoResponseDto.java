30e522f4bce5966a070221421c03acf77f6816ff
gdkim <oyammo@hanmail.net>
2021-06-23 13:06:29 +0900

    2021-06-23 / 프로덕트 dto 추가

---
+++ src/main/java/com/raonse2/pms_project/dto/product_info/Product_InfoResponseDto.java
@@ -0,0 +1,31 @@
+package com.raonse2.pms_project.dto.product_info;
+
+import com.raonse2.pms_project.model.Product_Info;
+import lombok.Getter;
+
+
+/**
+ * 고유 Entity는 건들면 안됨.
+ * 따로 DTO 객체 만들어서 전달.
+ */
+@Getter
+public class Product_InfoResponseDto {
+
+    private int productNo;
+    private String productName;
+    private int createEmpNo;
+    private int updateEmpNo;
+    private String productStandard;
+    private String productOptions;
+
+
+    public Product_InfoResponseDto(Product_Info entity){
+        this.productNo = entity.getProductNo();
+        this.productName = entity.getProductName();
+        this.createEmpNo = entity.getCreateEmpNo();
+        this.updateEmpNo = entity.getUpdateEmpNo();
+        this.productStandard = entity.getProductStandard();
+        this.productOptions = entity.getProductOptions();
+    }
+
+}
--- src/test/java/com/raonse2/pms_project/QuerydslTest.java
+++ src/test/java/com/raonse2/pms_project/QuerydslTest.java
@@ -1,8 +1,5 @@
 package com.raonse2.pms_project;
 
-import com.raonse2.pms_project.model.Product_Info;
-import com.raonse2.pms_project.querydslRepository.Product_InfoRepositorySupport;
-import com.raonse2.pms_project.repository.Product_InfoRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +11,11 @@ import java.util.List;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class QuerydslTest {
-    @Autowired
-    private Product_InfoRepository productInfoRepository;
+    //@Autowired
+    //private Product_InfoRepository productInfoRepository;
 
-    @Autowired
-    private Product_InfoRepositorySupport productInfoRepositorySupport;
+    //@Autowired
+    //private Product_InfoRepositorySupport productInfoRepositorySupport;
 
     /*@Test
     public void querydsl_기본기능(){
