53eaf7694a41742f8e46e9b04a4df2677b842e64
gdkim <oyammo@hanmail.net>
2021-08-17 17:12:08 +0900

    2021-08-17 / 7

---
+++ build/resources/main/static/js/selectAll.js
@@ -0,0 +1,8 @@
+
+function selectAll (obj)  {
+    const checkboxes = document.getElementsByName('check');
+
+    checkboxes.forEach((checkbox) => {
+        checkbox.checked = obj.checked;
+    })
+}
